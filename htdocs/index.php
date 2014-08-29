f5f2e1523e2b20917b52301ff02a53dfe000268a
Nicholas Avenell <nicholas@istic.net>
2014-08-29 13:26:52 +0200

    Fix bug in humanity detection explaination

--- htdocs/index.php
+++ htdocs/index.php
@@ -12,10 +12,20 @@ $error = false;
 
 if (isset($_POST['description'])){
 	if($_POST['description']){
+		if($_POST['captcha'] != 12){
+			$error = "Basic humanity test is required";
+		}
 		if($_POST['description'] != strip_tags($_POST['description'])){
 			$_POST['description'] = strip_tags($_POST['description']);
 			$error = "HTML is contraindicated, please review";
-		} else {
+		}
+
+		if($_POST['author'] != strip_tags($_POST['author'])){
+			$_POST['author'] = strip_tags($_POST['author']);
+			$error = "HTML is contraindicated, please review";
+		}
+
+		if($error === false){
 			$id = $item->create($_POST['author'], $_POST['description']);
 			header("location: /?entry=true");
 			die();
@@ -34,72 +44,20 @@ if(isset($_GET['uid'])){
 } else {
 	$item->fetch_random();
 }
+
+$f_contents = file("../lib/agents.txt"); 
+$agent = $f_contents[rand(0, count($f_contents) - 1)];
+$agent = ucwords($agent);
+
 ?><!DOCTYPE html>
 <html>
 <head>
-<title>The Basement - A Grue Liability Zone</title>
-<link href='http://fonts.googleapis.com/css?family=PT+Sans+Caption|Orbitron' rel='stylesheet' type='text/css'>
-<link href='style.css' type='text/css' rel='stylesheet'>
-<style type="text/css">
+	<title>The Basement - A Grue Liability Zone</title>
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans+Caption|Orbitron|Share+Tech+Mono' rel='stylesheet' type='text/css'>
+	<link href='style.css' type='text/css' rel='stylesheet'>
 
-
-</style>
-
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript">
-$("document").ready(function(){
-
-	$('nav a').click(function(){
-		if ($(this).hasClass("selected")){
-			return;
-		}
-		var showme = $("#"+$(this).attr("rel"));
-		$(".displayitem").slideUp("slow");
-		showme.slideDown("slow");
-		$('nav a').removeClass("selected");
-		$(this).addClass("selected");
-
-	});
-
-	$('.vote').click(function(){
-		if ($(this).hasClass("voteup")){
-			vote = "up";
-		} else {
-			vote = "down";
-		}
-
-		uid = $(this).attr("rel");
-
-		$.ajax({
-			url: "vote.php",
-			data: {uid: uid, vote: vote},
-			type: 'POST'
-		}).done(function(data) { 
-			$("#voted").html(data);
-		});
-
-	});
-
-	if ($('#item_added')){
-		$('#item_added').fadeIn();
-		setTimeout("hide_added()", 3000);
-	}
-
-<?PHP if($error){ ?>
-
-	$(".displayitem").hide();
-	$("#add").slideDown();
-
-<?PHP } ?>
-
-});
-
-function hide_added(){
-	$('#item_added').fadeOut('slow');
-}
-
-</script>
-
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" src="scripting.js"></script>
 </head>
 
 <body>
@@ -110,33 +68,37 @@ function hide_added(){
 		<a href="#" rel="item" class="selected">Item</a> | 
 		<a href="#" rel="about">About</a> | 
 		<a href="#" rel="add">Add Item</a> | 
-		<a href="/  id="random">Random</a>
+		<a href="/" id="random">Random</a>
 	</nav>
 </header>
 
 <?PHP
 
 if (isset($_GET['entry'])){
-	echo "<div id=\"item_added\" class=\"hidden\">Thanks, your entry has been added</div>";
+	echo "<div id=\"item_added\" class=\"hidden\">Submission acknowledged. Please await pickup, Agent $agent</div>";
 }
 ?>
 
 <div class="contentbox">
-<div id="item" class="displayitem">
-<p>
-	Inside a crate on level <?PHP  echo $item->level ?>, you find <br/>
-	<?PHP echo nl2br($item->description); ?>
-</p>
-<cite id="author"><? echo $item->author ?></cite>
 
-[ <a href="/?uid=<?PHP echo $item->uniqid?>" id="permalink" title="Link to this item">Link</a>
-| <span id="voted"> <a href="#" id="voteup" rel="<?PHP echo $item->uniqid?>" class="vote voteup">I like this</a>
-| <a href="#" id="votedown" rel="<?PHP echo $item->uniqid?>" class="vote votedown">I don't like this</a></span> ]
+<!--- Item Tab -->
+<div id="item" class="displayitem">
+	<p>
+		Inside a crate on level <span title="<?PHP echo $item->score ?>"><?PHP  echo $item->level ?></span>, you find <br/>
+		<?PHP echo nl2br($item->description); ?>
+	</p>
+	<cite id="author"><? echo $item->author ?></cite>
+
+	[ <a href="/?uid=<?PHP echo $item->uniqid?>" id="permalink" title="Link to this item">Link</a>
+	| <span id="voted"> <a href="#" id="voteup" rel="<?PHP echo $item->uniqid?>" class="vote voteup">I like this</a>
+	| <a href="#" id="votedown" rel="<?PHP echo $item->uniqid?>" class="vote votedown">I don't like this</a></span> ]
 
 </div>
 
+<!-- In a box on level 42, you find a form to add new items -->
+
 <form id="add" method="POST" action="." class="displayitem hidden">
-<h2>Add Item</h2>
+<h2>Warehouse form 23521/1&alpha; &mdash; New Acquisition</h2>
 
 <h3>Guidelines:</h3>
 
@@ -153,7 +115,7 @@ if (isset($_GET['entry'])){
 
 <?PHP
 	if($error){
-		printf('<p class="error">%s</p>', $error);
+		printf('<p id="error" class="error">%s</p>', $error);
 	}
 ?>
 
@@ -163,9 +125,16 @@ if (isset($_GET['entry'])){
 	<textarea cols="40" rows="6" name="description"><?PHP echo isset($_POST['description']) ? $_POST['description'] : '' ?></textarea>
 </div>
 
-<label>Author</label><input type="text" name="author" value="<?PHP echo isset($_POST['author']) ? $_POST['author'] : '' ?>"/>
+<div><label>Author</label><input type="text" name="author" value="<?PHP echo isset($_POST['author']) ? $_POST['author'] : '' ?>"/></div>
+<div><label>5 + 7 =</label><input type="number" name="captcha" value=""/> (Proving your humanity<sup>[1]</sup>)</div>
 <input type="submit" value="Insert" />
+
+<p><small>[1] This doesn't actually prove your humanity, just your ability to deal with integer numerical addition, but our research
+has suggested that this retards the progress of both artifical item generators and anyone with a maths degree, and that the
+latter usually get over it in time.</small></p>
 </form>
+
+<!-- In a book on level 23, you find out about the basement -->
 
 <div id="about"  class="displayitem hidden">
 <h2>About</h2>
@@ -173,10 +142,12 @@ if (isset($_GET['entry'])){
 
 <p>This is an attempt to do the same thing. We don't have the ten years of submissions the old one did, nor do we have the vast vault of imagination that the old one was seeded with. We do have vote buttons, though. </p>
 
-<p>This was created by <a href="mailto:nicholas+basement@istic.net">Nicholas Avenell</a> of <a href="http://istic.net">Istic.Networks</a>. It runs on stickyback plastic, string, PHP, MySQL &amp; the Amazon Cloud. Its favourite letter is v, and its favourite position is the cookie monster. It's not endorsed by <a href="http://www.sjgames.com">SJ Games</a> or anyone involved with the original <a href="http://warehouse23.com">Warehouse 23</a> basement, and we don't accept liablity for any distress caused by items you find in it.</p>
+<p>This was created by <a href="mailto:nicholas+basement@istic.net">Nicholas Avenell</a> of <a href="http://istic.net">Istic.Networks</a>. It runs on stickyback plastic, string, PHP, MySQL &amp; jQuery. Its favourite letter is v, and its favourite position is the cookie monster. It's not endorsed by <a href="http://www.sjgames.com">SJ Games</a> or anyone involved with the original <a href="http://warehouse23.com">Warehouse 23</a> basement, and we don't accept liablity for any distress caused by items you find in it.</p>
 </div>
 
 </div>
+
+<!-- In a box on level five, you find big brother, watching you -->
 
 <script type="text/javascript">
 
